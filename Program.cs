3b04a8fc6de0d612cc6ea6542838fa1912f1834d
Afif <afifjunihar@gmail.com>
2024-08-05 18:45:10 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("HITOPIA Test Problem");
+
+bool exitCode = false;
+while (!exitCode)
+{
+    Console.WriteLine("HITOPIA Test Problem");
+
+}
+
